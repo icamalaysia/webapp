9b8474684b7d679616e298f58653ac37d613f1a1
icamalaysia <114890932+icamalaysia@users.noreply.github.com>
2022-10-05 23:25:02 +0800

    commit

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `ICAMalaysia | Infection Control Assocition of Malaysia`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	url: `https://icamalaysia.netlify.app/`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
