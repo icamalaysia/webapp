ad5c23120a1c692eeaeb023ee57b8c831e7caecb
icamalaysia <114890932+icamalaysia@users.noreply.github.com>
2024-04-26 12:01:00 +0800

    remove footer

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -56,25 +56,25 @@ export const footerLists = [
 			},
 		],
 	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
-	},
+	// {
+	// 	title: 'Theme',
+	// 	items: [
+	// 		{
+	// 			title: 'Get Started',
+	// 			slug: '/theme/get-started',
+	// 		},
+	// 		{
+	// 			title: 'Style Guide',
+	// 			slug: '/theme/style-guide',
+	// 		},
+    //   {
+    //     title: 'Theme Setup',
+    //     slug: '/theme/theme-setup',
+    //   },
+    //   {
+    //     title: 'Customizing Odyssey',
+    //     slug: '/theme/customizing-odyssey',
+    //   }
+	// 	],
+	// },
 ];
